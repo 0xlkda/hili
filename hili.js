be04c80210cdd9d3264d023d9bd17886f046b8d8
0xlkda <lekhacduyanh@gmail.com>
2025-01-03 10:58:43 +0700

    update publish script

--- hili.js
+++ hili.js
@@ -60,5 +60,5 @@ let Hili = {
   },
   mark: (node, { transform }) => {
     TransformWalker({ transform }).walk(node)
-  }
+  },
 }
--- publish.sh
+++ publish.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-zip -r hili.zip ./ -x ".git*" ".vite/*" ".prettierrc" "publish.sh" "playground/*" "index.html"
+zip -r hili.zip  \
+  "manifest.json" \
+  "content.js" \
+  "hili.css" \
+  "hili.js" \
+  "hili.zip" \
+  "popup.css" \
+  "popup.html" \
+  "popup.js" \

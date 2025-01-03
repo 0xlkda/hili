cdba0fd6b3b55d0ce6e255d43574d0b93e9215c1
0xlkda <lekhacduyanh@gmail.com>
2025-01-03 15:28:36 +0700

    better UI

--- publish.sh
+++ publish.sh
@@ -2,10 +2,13 @@
 
 zip -r hili.zip  \
   "manifest.json" \
+  "reset.css" \
+  "tabs.css" \
+  "variables.css" \
   "content.js" \
   "hili.css" \
   "hili.js" \
   "hili.zip" \
   "popup.css" \
   "popup.html" \
-  "popup.js" \
+  "popup.js"
